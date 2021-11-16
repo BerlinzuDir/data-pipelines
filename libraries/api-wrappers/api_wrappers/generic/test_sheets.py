f39ea2b94dd327f70f6a74e154c331d2affd47e4
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-11-16 16:34:11 +0100

    fix flake8

--- libraries/api-wrappers/api_wrappers/generic/test_sheets.py
+++ libraries/api-wrappers/api_wrappers/generic/test_sheets.py
@@ -18,16 +18,25 @@ def expected_products() -> pd.DataFrame:
     return pd.DataFrame(
         {
             "id": [4600893900492, 4603514007105],
-            "name": ['Aromatisierter Vodka "Green Mark Rye" 40% vol.', 'Aromatisierter Vodka "Imperial Collection Gold" 40% vol'],
-            "ru name": ['Aроматизированная водка "Зеленая Марка", Ржаная" 40% алк.', 'Водка "Imperial Collection Gold" 40% алк.'],
+            "name": [
+                'Aromatisierter Vodka "Green Mark Rye" 40% vol.',
+                'Aromatisierter Vodka "Imperial Collection Gold" 40% vol'
+            ],
+            "ru name": [
+                'Aроматизированная водка "Зеленая Марка", Ржаная" 40% алк.',
+                'Водка "Imperial Collection Gold" 40% алк.'
+            ],
             "price": [7.99, 11.59],
             "tax": [19] * 2,
             "weight": [0.5] * 2,
             "unit": ['l'] * 2,
             "category": ['spirituosen'] * 2,
-            "tags": ['imported, online, wolt'] *2,
+            "tags": ['imported, online, wolt'] * 2,
             "supplier": ['Monolith'] * 2,
-            "pic": ['https://catalog.stolitschniy.shop/static/img/products/normalized/4600893900492.jpg', 'https://catalog.stolitschniy.shop/static/img/products/normalized/4603514007105.jpg'],
+            "pic": [
+                'https://catalog.stolitschniy.shop/static/img/products/normalized/4600893900492.jpg',
+                'https://catalog.stolitschniy.shop/static/img/products/normalized/4603514007105.jpg'
+            ],
             "description": ['Enthält Weizen und Roggen.', None],
         },
         index=[1, 2],
