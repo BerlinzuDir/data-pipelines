22b6e5e5568b7bcf3d5e213f7383a6c53d96b83d
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-12-02 23:07:35 +0100

    add test for proy generator

--- libraries/api-wrappers/api_wrappers/metro/scraper/proxy_generator/proxy_generator.py
+++ libraries/api-wrappers/api_wrappers/metro/scraper/proxy_generator/proxy_generator.py
@@ -1,11 +1,22 @@
+import ipaddress
+
 import requests
 from bs4 import BeautifulSoup
-from random import choice
+import random
 
 
-def proxy_generator():
+def proxy_generator(seed=None):
+    if seed:
+        random.seed(seed)
     response = requests.get("https://sslproxies.org/")
-    soup = BeautifulSoup(response.content, 'html5lib')
-    proxy = {'https': choice(list(map(lambda x: x[0] + ':' + x[1], list(
-        zip(map(lambda x: x.text, soup.findAll('td')[::8]), map(lambda x: x.text, soup.findAll('td')[1::8]))))))}
-    return proxy
+    soup = BeautifulSoup(response.content, 'html.parser')
+    proxy_ips = map(lambda x: x.text, soup.findAll('td')[::8])
+    proxy_ports = map(lambda x: x.text, soup.findAll('td')[1::8])
+    proxy_ips_ports = list(map(lambda x: x[0] + ':' + x[1], list(zip(proxy_ips, proxy_ports))))
+    while True:
+        proxy = {'https': random.choice(proxy_ips_ports)}
+        try:
+            ipaddress.ip_address(proxy["https"].split(":")[0])
+            return proxy
+        except ValueError as error:
+            print(error)
--- libraries/api-wrappers/api_wrappers/metro/scraper/proxy_generator/test_proxy_generator.py
+++ libraries/api-wrappers/api_wrappers/metro/scraper/proxy_generator/test_proxy_generator.py
@@ -1,2 +1,9 @@
+import pytest
+from proxy_generator import proxy_generator
+
+
+@pytest.mark.block_network
+@pytest.mark.vcr
 def test_proxy_generator():
-    assert False
+    proxy = proxy_generator(seed=1)
+    assert proxy == {'https': '54.37.160.90:1080'}
