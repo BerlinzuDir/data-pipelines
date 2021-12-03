aceea5fe4d5e84b1874adcad1f2bff2c24aa1641
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-12-03 03:25:42 +0100

    fix proxy generator test

--- libraries/api-wrappers/api_wrappers/metro/scraper/generators/proxies/test_proxy_generator.py
+++ libraries/api-wrappers/api_wrappers/metro/scraper/generators/proxies/test_proxy_generator.py
@@ -1,9 +1,10 @@
 import pytest
-from .proxy_generator import generate_proxy
+from api_wrappers.metro.scraper.generators.proxies.proxy_generator import ProxyGenerator
 
 
 @pytest.mark.block_network
 @pytest.mark.vcr
 def test_generate_proxy():
-    proxy = generate_proxy(seed=1)
-    assert proxy == {"https": "159.146.126.143:8080"}
+    proxy = ProxyGenerator()
+    proxy.reset_proxy(seed=1)
+    assert proxy.proxies == {"https": "159.146.126.143:8080"}
