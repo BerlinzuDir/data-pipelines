27e646b698b2bc40c7fa8022acb5d2dbaa103345
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-12-03 04:45:48 +0100

    fix flake

--- libraries/api-wrappers/api_wrappers/metro/scraper/scraper.py
+++ libraries/api-wrappers/api_wrappers/metro/scraper/scraper.py
@@ -179,7 +179,8 @@ def _get_eans(pdf_endpoint: str):
     keyword = "GTIN / EAN : "
     gtin_ean_index = pdf_content[0].find(keyword)
     zutat_index = pdf_content[0].find("\n\nZutat\n")
-    gtin_ean = pdf_content[0][int(gtin_ean_index + len(keyword)): zutat_index]
+    gtin_start_index = int(gtin_ean_index + len(keyword))
+    gtin_ean = pdf_content[0][gtin_start_index:zutat_index]
     if "," in gtin_ean:
         gtin_eans = gtin_ean.split(",")
     else:
