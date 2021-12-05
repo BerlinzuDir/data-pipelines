530ba9db6ab4a43d009e6796474ca4e8c1cfcd6d
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-12-05 09:34:39 +0100

    add intersection logic

--- libraries/api-wrappers/api_wrappers/metro/scraper/post_processor/post_processor.py
+++ libraries/api-wrappers/api_wrappers/metro/scraper/post_processor/post_processor.py
@@ -1,4 +1,5 @@
 import os
+
 import pandas as pd
 
 
@@ -9,8 +10,20 @@ def post_process_product_data(data_directory, excluded_brands, excluded_categori
             _drop_missing_eans).pipe(
             _drop_corrupt_eans).pipe(
             _exclude_brands, excluded_brands=excluded_brands).pipe(
-            _exclude_categories, excluded_categories=excluded_categories)
+            _exclude_categories, excluded_categories=excluded_categories).pipe(
+            _drop_duplicates
+        )
     )
+
+
+def remove_intersections(df_main: pd.DataFrame, df_sub: pd.DataFrame) -> pd.DataFrame:
+    df_main_ids = set(df_main["Id"].values)
+    df_sub_ids = set(df_sub["Id"].values)
+    intersection = df_main_ids.intersection(df_sub_ids)
+    return {
+        "df_main": df_main[~df_main["Id"].isin(intersection)],
+        "df_sub": df_sub,
+    }
 
 
 def _load_data(data_directory: str) -> pd.DataFrame:
@@ -58,13 +71,16 @@ def _exclude_categories(products: pd.DataFrame, excluded_categories: list) -> pd
     return products
 
 
+def _drop_duplicates(products: pd.DataFrame) -> pd.DataFrame:
+    return products.drop_duplicates()
+
+
 if __name__ == '__main__':
-    DATA_NAME = ""
-    DATA_DIRECTORY = f'api_wrappers/metro/data/{DATA_NAME}'
+    DATA_NAMES = {"df_main": "filtered_by_category", "df_sub": "bio_query"}
     EXCLUDED_BRANDS = [
         "metro chef",
         "aro",
-        "metro chef bio"
+        "metro chef bio",
         "metro premium",
         "metro chef gourvenience",
     ]
@@ -79,9 +95,17 @@ if __name__ == '__main__':
         "ostern",
         "wein",
     ]
-    products = post_process_product_data(
-        DATA_DIRECTORY,
-        excluded_brands=EXCLUDED_BRANDS,
-        excluded_categories=EXCLUDED_CATEGORIES_KEYWORDS
-    )
-    products.to_csv(f'products_{DATA_NAME}.csv')
+
+    df_dict = {}
+    for key, name in DATA_NAMES.items():
+        data_directory = f'api_wrappers/metro/data/{name}'
+        df_dict[key] = post_process_product_data(
+            data_directory,
+            excluded_brands=EXCLUDED_BRANDS,
+            excluded_categories=EXCLUDED_CATEGORIES_KEYWORDS
+        )
+
+    df_dict = remove_intersections(**df_dict)
+
+    for key, name in DATA_NAMES.items():
+        df_dict[key].to_csv(f'products_{name}.csv')
