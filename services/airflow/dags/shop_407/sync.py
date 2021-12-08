d56fba42183e69e2a02fdfa42598d688866765b2
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-12-08 04:28:22 +0100

    first draft shop 407

---
+++ services/airflow/dags/shop_407/sync.py
@@ -0,0 +1,68 @@
+import json
+
+import pandas as pd
+import pathlib
+from api_wrappers.google import get_product_data_from_sheets
+from api_wrappers.google.google_sheets import get_default_category_mapping
+from api_wrappers.lozuka.lozuka_api import post_articles
+import ramda as R
+
+
+TRADER_ID = "407"
+FTP_ENDPOINT = "http://s739086489.online.de/bzd-bilder"
+GOOGLE_SHEETS_ADDRESS = "1_wRovPxM810eGCseDIga-N5iI7dwrSnlsWrItw17O8c"
+GOOGLE_DRIVE_ADDRESS = "1xLbWEWLnF4-niD9mLqMfPTkM87w8AVxG"
+
+
+def product_pipeline():
+    return R.pipe(
+        _load_product_data,
+        _transform_product_data,
+        post_articles(_load_credentials("/shop-secrets.json"), TRADER_ID),
+    )("")
+
+
+def _load_credentials(filename: str):
+    with open(_get_path_of_file() + filename) as secrets_json:
+        return json.load(secrets_json)
+
+
+def _get_path_of_file() -> str:
+    return str(pathlib.Path(__file__).parent.resolve())
+
+
+def _load_product_data(*args) -> pd.DataFrame:
+    return get_product_data_from_sheets(GOOGLE_SHEETS_ADDRESS)
+
+
+def _transform_product_data(products: pd.DataFrame):
+    products["Bruttopreis"] = products["Bruttopreis"].apply(
+        lambda price: R.pipe(
+            lambda val: val[1:],
+            lambda val: val.replace(",", "."),
+            lambda val: float(val),
+        )(price)
+    )
+
+    mapping = get_default_category_mapping()
+
+    products["Kategorie"] = products["Kategorie"].apply(
+        lambda category_name: _map_product_category(mapping, category_name)
+    )
+    products["Produktbild \n(Dateiname oder url)"] = (
+        f"{FTP_ENDPOINT}/{TRADER_ID}/" + products["Produktbild \n(Dateiname oder url)"]
+    )
+    return products
+
+
+@R.curry
+def _map_product_category(mapping: pd.DataFrame, category_name: str) -> int:
+    return R.pipe(
+        lambda df: df[df["category_name"] == category_name].astype({"category_id": str}),
+        lambda df: df["category_id"].values,
+        R.if_else(lambda x: len(x) == 1, list, lambda: raise_value_error("Invalid category")),
+    )(mapping)
+
+
+def raise_value_error(message):
+    raise ValueError(message)
