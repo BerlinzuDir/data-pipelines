f9db9d4dd5010608629651dd40d1191aa43a9465
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-12-08 21:25:20 +0100

    integrate shop

---
+++ services/airflow/dags/shop_381/sync_images/test_sync_images.py
@@ -0,0 +1,22 @@
+from .sync_images import _load_sftp_credentials_from_env, _connect_to_sftp, load_files_from_google_to_sftp
+from ..sync import TRADER_ID
+
+
+def test_load_files_from_google_to_sftp():
+    load_files_from_google_to_sftp(STORE_ID, FOLDER_ID)
+
+    assert file_exists_on_sftp("1.png")
+    assert file_exists_on_sftp("2.jpeg")
+
+
+def file_exists_on_sftp(filename):
+    credentials = _load_sftp_credentials_from_env()
+    with _connect_to_sftp(credentials) as client:
+        exists = filename in client.listdir(f"bzd/{STORE_ID}")
+        if exists:
+            client.remove(f"bzd/{STORE_ID}/{filename}")
+    return exists
+
+
+STORE_ID = f"{TRADER_ID}_test"
+FOLDER_ID = "1lQ2dyF3bschhZIl4MdMZ-Bn0VmbEz5Qv"
