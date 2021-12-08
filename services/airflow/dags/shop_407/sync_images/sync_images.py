5f84298996b05a34897f1c1071882843858b774a
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-12-08 15:12:25 +0100

    include download from drive method in sync_images

--- services/airflow/dags/shop_407/sync_images/sync_images.py
+++ services/airflow/dags/shop_407/sync_images/sync_images.py
@@ -1,13 +1,12 @@
 from contextlib import contextmanager
-import requests
-import shutil
+
 import pandas as pd
 from typing import TypedDict, List
 import paramiko
-from time import sleep
 import os
 import ramda as R
 
+from api_wrappers.google.google_drive import download_file_from_drive
 from dags.helpers.decorators import cwd_cleanup
 from api_wrappers.google import get_file_list_from_drive
 
@@ -74,21 +73,5 @@ def _load_single_image_to_sftp(sftp_client, store_id: int, filename: str) -> str
 
 
 def _download_all_files(file_list: pd.DataFrame) -> pd.DataFrame:
-    file_list[["link", "title"]].apply(lambda row: _download(row["link"], row["title"]), axis=1)
+    file_list[["id", "title"]].apply(lambda row: download_file_from_drive(row["id"], row["title"]), axis=1)
     return file_list
-
-
-def _download(url: str, filename: str) -> None:
-    while not os.path.isfile(filename):
-
-        response = requests.get(url, stream=True)
-
-        # Check if the image was retrieved successfully
-        response.raise_for_status()
-        # Set decode_content value to True, otherwise the downloaded image file's size will be zero.
-        response.raw.decode_content = True
-
-        # Open a local file with wb ( write binary ) permission.
-        with open(filename, "wb") as f:
-            shutil.copyfileobj(response.raw, f)
-        sleep(1)
