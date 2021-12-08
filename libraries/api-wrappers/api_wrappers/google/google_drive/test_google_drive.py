26cd5c7106eb6924ac595b752b222d23d5296941
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-12-08 15:12:01 +0100

    add download google drive files logic

--- libraries/api-wrappers/api_wrappers/google/google_drive/test_google_drive.py
+++ libraries/api-wrappers/api_wrappers/google/google_drive/test_google_drive.py
@@ -1,12 +1,17 @@
+import os
+import shutil
+import numpy as np
+
 import pandas as pd
 import pytest
 import requests
 from pydrive2.files import ApiRequestError
 
-from api_wrappers.google.google_drive import get_file_list_from_drive
+from api_wrappers.google.google_drive import get_file_list_from_drive, download_file_from_drive
 
 FOLDER_ID = "1lQ2dyF3bschhZIl4MdMZ-Bn0VmbEz5Qv"
 FOLDER_ID_NOT_SHARED = "0B6Eu5C6m7kAtcmlHVXpqTmpJUk0"
+FILE_ID = '1ym44i-TWgTHu5Ncd9XIQjS4UIKdCAOfa'
 
 
 def test_get_file_list_from_drive():
@@ -17,12 +22,7 @@ def test_get_file_list_from_drive():
 
     # file list has correct length
     assert len(file_list) == 2
-
-    # download links are working
-    assert requests.get(file_list.link[0]).status_code == 200
-    assert requests.get(file_list.link[1]).status_code == 200
-
-    # hash contains hash
+    assert file_list.id[0] == FILE_ID
     assert file_list.hash[0] == "15bdf97b0e2de0293ec02720e25144ec"
 
 
@@ -35,3 +35,22 @@ def test_get_file_list_with_wrong_or_restricted_folder():
         _ = get_file_list_from_drive(FOLDER_ID_NOT_SHARED)
 
     assert "No files returned from server. Make sure, folder id is correct and folder is shared." in str(err)
+
+
+def test_download_file_from_drive(_filename):
+    download_file_from_drive(FILE_ID, _filename)
+    assert os.path.getsize(_filename) == 4560
+
+
+@pytest.fixture
+def _filename():
+    directory = "dir" + str(np.random.randint(10000, 99999))
+    os.mkdir(directory)
+    os.chdir(directory)
+    try:
+        yield 'test.png'
+    except Exception as error:
+        raise error
+    finally:
+        os.chdir("../")
+        shutil.rmtree(directory)
