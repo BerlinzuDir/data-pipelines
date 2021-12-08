4cc656482fe6fdb218ad154b0a9fcf69bcab1142
KilianZimmerer <21954712+KilianZimmerer@users.noreply.github.com>
2021-12-08 16:08:41 +0100

    Feature/cleanup cwd after image sync (#35)
    * wip: add airflow to poetry env, add template files
    
    add sync and test
    
    fix flake
    
    fix flake8
    
    unique module name for tests
    
    add shop secret
    
    only temporar: include image download
    
    fix image_url
    
    * fix ftp image url and add wip of image transfer
    
    * remove obsolete ftp endpoint
    
    * move global variables down
    
    * setup image sync
    
    * fix sftp port assignement
    
    * finalize sync_images
    
    * tests of sync_images
    
    * add test for sync
    
    * black formatting
    
    * rename tests and adapt casette
    
    * delete casette
    
    * add fischkonserven category
    
    * start_date of dags to 0 days
    
    * Add Port to env file
    
    * black formatting
    
    * improve pytest.fixture
    
    * add decorator to cleanup downloads
    
    * black formatting
    
    * update README
    
    * fix tests
    
    * finalize get articles
    
    * black formatting
    
    * change start_date of dag to 1
    
    * add test for sync images with sftp
    
    * finalize test for image sync
    
    * fix flake
    
    * black formatting
    
    * include test and TODO
    
    * fix return value of get_articles
    
    * add secrets
    
    * fix sync_images
    
    * black formatting
    
    * rename cwd cleanup decorator
    
    * add cleanup decorator and fix tests
    
    * fix tests
    
    Co-authored-by: KilianZimmerer <kilian.zimmerer@gmail.com>

--- services/airflow/dags/shop_287/sync_images/test_sync_images.py
+++ services/airflow/dags/shop_287/sync_images/test_sync_images.py
@@ -1,8 +1,7 @@
 import os.path
-from shutil import rmtree
-import pytest
-import numpy as np
 import pandas as pd
+
+from dags.helpers.decorators import cwd_cleanup
 from .sync_images import (
     _download,
     _load_single_image_to_sftp,
@@ -13,14 +12,14 @@ from .sync_images import (
 )
 
 
-def test_load_files_from_google_to_sftp(clean_cwd):
+def test_load_files_from_google_to_sftp():
     load_files_from_google_to_sftp(STORE_ID, FOLDER_ID)
 
-    assert os.path.isfile(GOOGLE_FILENAME)
     assert file_exists_on_sftp(GOOGLE_FILENAME)
 
 
-def test_download_all(clean_cwd):
+@cwd_cleanup
+def test_download_all():
     file_list = _download_all_files(FILE_LIST)
 
     pd.testing.assert_frame_equal(file_list, FILE_LIST)
@@ -28,12 +27,14 @@ def test_download_all(clean_cwd):
     assert os.path.isfile(FILENAME2)
 
 
-def test_download_file(clean_cwd):
+@cwd_cleanup
+def test_download_file():
     _download(IMAGE_URL1, FILENAME1)
     assert os.path.isfile(FILENAME1)
 
 
-def test_load_to_sftp(clean_cwd):
+@cwd_cleanup
+def test_load_to_sftp():
     _download(IMAGE_URL1, FILENAME1)
     assert os.path.isfile(FILENAME1)
 
@@ -51,18 +52,6 @@ def file_exists_on_sftp(filename):
         if exists:
             client.remove(f"bzd/{STORE_ID}/{filename}")
     return exists
-
-
-@pytest.fixture
-def clean_cwd():
-    directory = "dir" + str(np.random.randint(10000, 99999))
-    os.mkdir(directory)
-    os.chdir(directory)
-    try:
-        yield directory
-    finally:
-        os.chdir("../")
-        rmtree(directory)
 
 
 STORE_ID = 1234
