5051d46a5abea4f68b16e7117cdd54a5b38bee6c
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-12-10 17:26:28 +0100

    first draft: adapt image sync

--- services/airflow/dags/shop_407/sync_images/sync_images.py
+++ services/airflow/dags/shop_407/sync_images/sync_images.py
@@ -6,25 +6,11 @@ import paramiko
 import os
 import ramda as R
 
+from api_wrappers.google import get_product_data_from_sheets
 from api_wrappers.google.google_drive import download_file_from_drive
 from dags.helpers.decorators import cwd_cleanup
 from api_wrappers.google import get_file_list_from_drive
-
-
-class FileListDict(TypedDict):
-    title: List[str]
-    url: List[str]
-    hash: List[str]
-
-
-@cwd_cleanup
-def load_files_from_google_to_sftp(store_id: str, google_drive_folder_id: str) -> None:
-    R.pipe(
-        get_file_list_from_drive,
-        _download_all_files,
-        _load_all_files_to_sftp(store_id),
-        lambda df: df.to_dict(),  # return values have to be json serializable
-    )(google_drive_folder_id)
+from dags.shop_407.sync import GOOGLE_SHEETS_ADDRESS, GOOGLE_DRIVE_ADDRESS
 
 
 class FtpCredentials(TypedDict):
@@ -34,13 +20,42 @@ class FtpCredentials(TypedDict):
     port: int
 
 
-def _load_sftp_credentials_from_env() -> FtpCredentials:
-    return FtpCredentials(
-        username=os.environ["FTP_USER"],
-        password=os.environ["FTP_PASSWORD"],
-        hostname=os.environ["FTP_HOSTNAME"],
-        port=int(os.environ["FTP_PORT"]),
-    )
+@cwd_cleanup
+def load_images_to_sftp(store_id: str) -> pd.DataFrame:
+    products = _load_product_data()
+    R.pipe(
+        _get_file_list(GOOGLE_DRIVE_ADDRESS),
+        _download_all_files,
+        _load_all_files_to_sftp(store_id),
+        lambda df: df.to_dict(),  # return values have to be json serializable
+    )(products)
+    return products
+
+
+def _load_product_data() -> pd.DataFrame:
+    return get_product_data_from_sheets(GOOGLE_SHEETS_ADDRESS)
+
+
+@R.curry
+def _get_file_list(google_drive_adress: str, products: pd.DataFrame) -> pd.DataFrame:
+    file_list_drive = get_file_list_from_drive(google_drive_adress)
+    products = products.rename(columns={"ID": "title"})
+    file_list_drive["title"] = file_list_drive["title"].str.replace('JPG', 'jpg')
+    products["Produktbild \n(Dateiname mit Endung)"] = products["Produktbild \n(Dateiname mit Endung)"].str.replace('JPG', 'jpg').str.replace('PNG', 'png')
+    not_assigned_images_sheet = set(products["Produktbild \n(Dateiname mit Endung)"].values).difference(file_list_drive["title"].values)
+    not_assigned_images_drive = set(file_list_drive["title"].values).difference(products["Produktbild \n(Dateiname mit Endung)"].values)
+    if not_assigned_images_sheet or not_assigned_images_drive:
+        # TODO: send notification and log warning
+        products = products[~products["Produktbild \n(Dateiname mit Endung)"].isin(not_assigned_images_sheet)]
+    products["title"] = products["title"].astype(str)
+    products = products.merge(file_list_drive, how='left', left_on="Produktbild \n(Dateiname mit Endung)", right_on='title')
+    products["title"] = products["title_x"] + products["title_y"].apply(lambda x: '.' + x.split('.')[-1])
+    return products[["id", "title"]]
+
+
+def _download_all_files(file_list: pd.DataFrame) -> pd.DataFrame:
+    file_list[["id", "title"]].apply(lambda row: download_file_from_drive(row["id"], row["title"]), axis=1)
+    return file_list
 
 
 @R.curry
@@ -49,6 +64,15 @@ def _load_all_files_to_sftp(store_id: str, file_list: pd.DataFrame) -> pd.DataFr
     with _connect_to_sftp(credentials) as sftp_client:
         file_list["title"].apply(lambda title: _load_single_image_to_sftp(sftp_client, store_id, title))
     return file_list
+
+
+def _load_sftp_credentials_from_env() -> FtpCredentials:
+    return FtpCredentials(
+        username=os.environ["FTP_USER"],
+        password=os.environ["FTP_PASSWORD"],
+        hostname=os.environ["FTP_HOSTNAME"],
+        port=int(os.environ["FTP_PORT"]),
+    )
 
 
 @contextmanager
@@ -70,8 +94,3 @@ def _load_single_image_to_sftp(sftp_client, store_id: str, filename: str) -> str
         sftp_client.mkdir(f"bzd/{store_id}/")
     sftp_client.put(filename, f"bzd/{store_id}/{filename}")
     return filename
-
-
-def _download_all_files(file_list: pd.DataFrame) -> pd.DataFrame:
-    file_list[["id", "title"]].apply(lambda row: download_file_from_drive(row["id"], row["title"]), axis=1)
-    return file_list
--- services/airflow/dags/shop_407/sync_images/test_sync_images.py
+++ services/airflow/dags/shop_407/sync_images/test_sync_images.py
@@ -1,12 +1,39 @@
-from .sync_images import _load_sftp_credentials_from_env, _connect_to_sftp, load_files_from_google_to_sftp
-from dags.shop_407.sync import TRADER_ID
+from unittest import TestCase
+
+import pytest
+
+from dags.shop_407.sync_images import sync_images
+from .sync_images import _load_sftp_credentials_from_env, _connect_to_sftp, load_images_to_sftp
+
+from dags.shop_407.sync import TRADER_ID, _load_product_data
 
 
-def test_load_files_from_google_to_sftp():
-    load_files_from_google_to_sftp(STORE_ID, FOLDER_ID)
+TESTCASE = TestCase()
+STORE_ID = f"{TRADER_ID}_test"
+FOLDER_ID = "1lQ2dyF3bschhZIl4MdMZ-Bn0VmbEz5Qv"
 
-    assert file_exists_on_sftp("1.png")
-    assert file_exists_on_sftp("2.jpeg")
+
+def test_load_images_to_sftp(_decorate_load_product_data, _sftp_cleanup):
+    products = load_images_to_sftp(STORE_ID)
+
+    assert len(products) == 4
+    assert file_exists_on_sftp('1330.jpg')
+    assert file_exists_on_sftp("1459.jpg")
+    assert file_exists_on_sftp('1578.jpg')
+    assert file_exists_on_sftp("1593.jpg")
+
+
+@pytest.fixture
+def _decorate_load_product_data():
+    """Cut the return dataframe of _load_product_data to shorten test run."""
+
+    def dec(func):
+        def inner():
+            return func()[:4]
+
+        return inner
+
+    sync_images._load_product_data = dec(_load_product_data)
 
 
 def file_exists_on_sftp(filename):
@@ -18,5 +45,11 @@ def file_exists_on_sftp(filename):
     return exists
 
 
-STORE_ID = f"{TRADER_ID}_test"
-FOLDER_ID = "1lQ2dyF3bschhZIl4MdMZ-Bn0VmbEz5Qv"
+@pytest.fixture
+def _sftp_cleanup():
+    yield None
+    credentials = _load_sftp_credentials_from_env()
+    with _connect_to_sftp(credentials) as client:
+        for file in client.listdir(f"bzd/{STORE_ID}"):
+            client.remove(f"bzd/{STORE_ID}/{file}")
+        client.rmdir(f"bzd/{STORE_ID}")
