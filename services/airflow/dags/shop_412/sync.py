3ad4db7a4d9d78171f7dbad43578d673936d5726
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-12-13 02:23:56 +0100

    integrate trader 412

---
+++ services/airflow/dags/shop_412/sync.py
@@ -0,0 +1,67 @@
+import json
+
+import pandas as pd
+import pathlib
+from api_wrappers.google.google_sheets import get_default_category_mapping
+from api_wrappers.lozuka.lozuka_api import post_articles
+import ramda as R
+
+
+TRADER_ID: str = "412"
+FTP_ENDPOINT = "http://s739086489.online.de/bzd-bilder/bzd"
+GOOGLE_SHEETS_ADDRESS = "1e6bvl7Rb075bjd1rN6X_hexiaawowSSNmIphG9pZp5g"
+GOOGLE_DRIVE_ADDRESS = "1QT0oswEcSBrubzQQ3ysAzpAeu86vNr3t"
+
+
+def product_pipeline(products: json):
+    R.pipe(
+        _from_json_records,
+        _set_bruttopreis,
+        _category_mapping,
+        post_articles(_load_credentials("/shop-secrets.json"), TRADER_ID),
+    )(products)
+
+
+def _from_json_records(products: str) -> pd.DataFrame:
+    return pd.DataFrame.from_records(json.loads(products))
+
+
+def _load_credentials(filename: str) -> dict:
+    with open(_get_path_of_file() + filename) as secrets_json:
+        return json.load(secrets_json)
+
+
+def _get_path_of_file() -> str:
+    return str(pathlib.Path(__file__).parent.resolve())
+
+
+def _set_bruttopreis(products: pd.DataFrame) -> pd.DataFrame:
+    products["Bruttopreis"] = products["Bruttopreis"].apply(
+        lambda price: R.pipe(
+            lambda val: val[1:],
+            lambda val: val.replace(",", "."),
+            lambda val: float(val),
+        )(price)
+    )
+    return products
+
+
+def _category_mapping(products: pd.DataFrame) -> pd.DataFrame:
+    mapping = get_default_category_mapping()
+    products["Kategorie"] = products["Kategorie"].apply(
+        lambda category_name: _map_product_category(mapping, category_name)
+    )
+    return products
+
+
+@R.curry
+def _map_product_category(mapping: pd.DataFrame, category_name: str) -> int:
+    return R.pipe(
+        lambda df: df[df["category_name"] == category_name].astype({"category_id": str}),
+        lambda df: df["category_id"].values,
+        R.if_else(lambda x: len(x) == 1, list, lambda: raise_value_error("Invalid category")),
+    )(mapping)
+
+
+def raise_value_error(message):
+    raise ValueError(message)
---
+++ services/airflow/dags/shop_412/sync_images/__init__.py
@@ -0,0 +1,3 @@
+from .sync_images import load_images_to_sftp
+
+__all__ = ["load_images_to_sftp"]
