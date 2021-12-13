548ec1ff46d2f310afcb93a904fd513e919b420b
KilianZimmerer <kilian.zimmerer@gmail.com>
2021-12-13 02:37:36 +0100

    black formatting

--- services/airflow/dags/shop_412/test_sync.py
+++ services/airflow/dags/shop_412/test_sync.py
@@ -70,11 +70,11 @@ PRODUCTS_INPUT = (
     ',"3":7},"Ma\\u00dfeinheit":{"0":"kg","1":"stk","2":"stk","3":"kg"},"Verpackungsgr\\u00f6\\u00dfe":{"0":1,"1":1,"2'
     '":1,"3":1},"Kategorie":{"0":"Obst Gem\\u00fcse","1":"Obst Gem\\u00fcse","2":"Obst Gem\\u00fcse","3":"Obst Gem\\u0'
     '0fcse"},"R\\u00fcckgabe M\\u00f6glich":{"0":"nein","1":"nein","2":"nein","3":"nein"},"K\\u00fchlpflichtig":{"0":"'
-    '","1":" ","2":"","3":""},"Produktbild \\n(Dateiname oder url)":{"0":"http:\\/\\/s739086489.online.de\\/bzd-bilder'
-    '\\/bzd\\/412\\/1.jpg","1":"http:\\/\\/s739086489.online.de\\/bzd-bilder\\/bzd\\/412\\/2.jpg","2":"http:\\/\\/s739'
-    '086489.online.de\\/bzd-bilder\\/bzd\\/412\\/3.jpg","3":"http:\\/\\/s739086489.online.de\\/bzd-bilder\\/bzd\\/412\
-    \/4."},"Bestand":{"0":"","1":"","2":"","3":""},"Ma\\u00dfeinheit \\nf\\u00fcr Bestand":{"0":"","1":"","2":"","3":"'
-    '"},"GTIN\\/EAN":{"0":"","1":"","2":"","3":""},"ISBN":{"0":"","1":"","2":"","3":""},"SEO \\nkeywords":{"0":"","1":'
-    '"","2":"","3":""},"SEO \\nBeschreibungstext":{"0":"","1":"","2":"","3":""},"SEO \\nSeitentitel":{"0":"","1":"","2'
-    '":"","3":""}}'
+    '","1":" ","2":"","3":""},"Produktbild \\n(Dateiname oder url)":{"0":"http:\\/\\/s739086489.online.de\\/bzd-bild'
+    'er\\/bzd\\/412\\/1.jpg","1":"http:\\/\\/s739086489.online.de\\/bzd-bilder\\/bzd\\/412\\/2.jpg","2":"http:\\/\\/s'
+    '739086489.online.de\\/bzd-bilder\\/bzd\\/412\\/3.jpg","3":"http:\\/\\/s739086489.online.de\\/bzd-bilder\\/bzd\\/'
+    '412\\/4."},"Bestand":{"0":"","1":"","2":"","3":""},"Ma\\u00dfeinheit \\nf\\u00fcr Bestand":{"0":"","1":"","2":""'
+    ',"3":""},"GTIN\\/EAN":{"0":"","1":"","2":"","3":""},"ISBN":{"0":"","1":"","2":"","3":""},"SEO \\nkeywords":{"0":'
+    '"","1":"","2":"","3":""},"SEO \\nBeschreibungstext":{"0":"","1":"","2":"","3":""},"SEO \\nSeitentitel":{"0":"","'
+    '1":"","2":"","3":""}}'
 )
